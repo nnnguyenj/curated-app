1e3a087ac2467dd6feda5dcf0b21c175a4633521
emma-ylagan <emma@ylagan.com>
2025-05-28 16:03:02 -0400

    Server / Project up and running

---
+++ backend/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all().order_by('-created_at')
+    serializer_class = ItemSerializer
